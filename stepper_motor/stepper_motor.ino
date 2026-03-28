fca4b0ce5f5a8b2f7dca35fea587ba38bd8054d6
Anna Hudson <anna.hudson356@gmail.com>
2026-03-28 12:12:13 -0400

    Added slow revolution for testing

--- stepper_motor/stepper_motor.ino
+++ stepper_motor/stepper_motor.ino
@@ -1,17 +1,36 @@
-#
 #include <AccelStepper.h>
 
 #define STEP_PIN 3
 #define DIR_PIN 2
 #define M0_PIN 7
-#
+#define M1_PIN 6
+#define M2_PIN 5
+
+// Step Angle 1.8 degrees per revolution
+const float stepsPerRev = 200;
+
+AccelStepper stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(DIR_PIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(DIR_PIN, HIGH);
+
+  for(int i = 0; i < stepPerRev; i++){
+      digitalWrite(STEP_PIN, HIGH);
+      delayMicroseconds(2000);
+      digitalWrite(STEP_PIN, LOW);
+      delayMicroseconds(2000);
+  }
+  delay(1000);
+
+  digitalWrite(DIR_PIN, LOW);
 
 }
