215a9cbd7a4e291b42716195c4ed1df8bf131bf3
Anna Hudson <anna.hudson356@gmail.com>
2026-03-28 11:35:38 -0400

    added stepper motor file

---
+++ stepper_motor/stepper_motor.ino
@@ -0,0 +1,17 @@
+#
+#include <AccelStepper.h>
+
+#define STEP_PIN 3
+#define DIR_PIN 2
+#define M0_PIN 7
+#
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
